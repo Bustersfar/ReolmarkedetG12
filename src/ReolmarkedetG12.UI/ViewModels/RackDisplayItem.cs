ac14141f1832bd5f826084804f02b85f6a78650d
benjiens1985 <jens9226@stud.kea.dk>
2026-09-21 16:17:31 +0200

    RackView: flervalg af reoler, kundens reoler-boks, Viewbox-skalering

---
+++ src/ReolmarkedetG12.UI/ViewModels/RackDisplayItem.cs
@@ -0,0 +1,21 @@
+﻿using ReolmarkedetG12.Core.Models;
+using ReolmarkedetG12.UI.MVVM;
+
+namespace ReolmarkedetG12.UI.ViewModels;
+
+public class RackDisplayItem : ViewModelBase
+{
+    public Rack Rack { get; }
+
+    private bool _isSelected;
+    public bool IsSelected
+    {
+        get => _isSelected;
+        set { _isSelected = value; OnPropertyChanged(); }
+    }
+
+    public RackDisplayItem(Rack rack)
+    {
+        Rack = rack;
+    }
+}
